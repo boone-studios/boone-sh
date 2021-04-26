1fb5424043756f26d2f7829058420aea5e4a2bac
Nathanael McDaniel <leemcd56@gmail.com>
2021-04-26 17:39:37 -0500

    Added landing page to describe domain

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <landing />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Landing,
   }
 })
 </script>
@@ -20,8 +19,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="relative py-16 bg-white overflow-hidden">
+    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+      <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+        <svg class="absolute top-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+          <defs>
+            <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+        </svg>
+
+        <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+          <defs>
+            <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+        </svg>
+
+        <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+          <defs>
+            <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+        </svg>
+      </div>
+    </div>
+
+    <div class="relative px-4 sm:px-6 lg:px-8">
+      <div class="text-lg max-w-prose mx-auto">
+        <h1>
+          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Introducing</span>
+          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">API Resource Farm</span>
+        </h1>
+
+        <p class="mt-8 text-xl text-gray-500 leading-8">We spend 60% of our time building various APIs to work with our products, and many times struggle to stay uniform in choosing endpoint locations.</p>
+      </div>
+
+      <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+        <p>To solve this, we chose this ubiquitous domain to act as a resource factory where all of our APIs can utilize this domain as subdomains. By doing this, we solve the following problems:</p>
+
+        <ul>
+          <li>One location to propogate APIs</li>
+          <li>Easier to mass configure domain security</li>
+          <li>Branded domain, so backtracing resource calls can help us identify points of failure</li>
+        </ul>
+
+        <p>If you have an ongoing or existing project with us, odds are that your API is using this domain.</p>
+      </div>
+
+      <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+        <p>If you curious as to what we do, or are interested in working with us, visit us on our website at <a href="https://boone.io" rel="nofollow noreferer" target="_blank">https://boone.io</a> or via the phone number below:</p>
+
+        <p>
+          <strong>Toll Free:</strong> (601) 550-6560 <br />
+          Mon thru Fri, 9AM to 5PM (CDT)
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
